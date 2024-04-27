8ced6e678efdb4f43d206b92816d51d989f40951
inhorinrin <167855513+inhorinrin@users.noreply.github.com>
2024-04-27 15:57:36 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,48 @@
+ let canva = document.getElementById("canvas")
+        canva.innerHTML="<h1 id='plobrem'>問題</h1><h2 id='roma'>mondai</h2>"
+
+        let roma=""
+        let kanji=""
 
 
-let canva = document.getElementById("canvas")
-canva.innerHTML="<h1 id='plobrem'>問題</h1><h2 id='roma'>mondai</h2>"
+        function putForCanvas(html){
+            let canva = document.getElementById("canvas")
+            canva.innerHTML=html        
+        }
 
-let roma=""
-let kanji=""
+        function showThePloblem(kanjif,romaf){
+            let canvaf = document.getElementById("plobrem")
+            let canvas = document.getElementById("roma")
+            canvaf.innerHTML=kanjif
+            canvas.innerHTML=romaf
+        }
 
 
-function putForCanvas(html){
-    let canva = document.getElementById("canvas")
-    canva.innerHTML=html        
-}
 
-function showThePloblem(kanjif,romaf){
-    let canvaf = document.getElementById("plobrem")
-    let canvas = document.getElementById("roma")
-    canvaf.innerHTML=kanjif
-    canvas.innerHTML=romaf
-}
-
-function showingtest(first,second){
-    showThePloblem(first,second)
-    roma =second
-    kanji=first
-}
-
-function keypress_ivent(e) {
-    if (e.key == roma[0]){
-        roma = roma.slice(1);
-        showThePloblem(kanji,roma)              
-    }
+        function showingtest(first,second){
+            showThePloblem(first,second)
+            roma =second
+            kanji=first
+        }
         
-    
-}
+        let plobrem_index = 0
+        let plobrems=[["みかん","mikan"],["新羽島","shinhashima"]]
 
-document.addEventListener('keypress', keypress_ivent);
+        function nextPlobrem(){
+            showingtest(plobrems[plobrem_index][0],plobrems[plobrem_index][1])
+            plobrem_index++
+        }
+
+        function keypress_ivent(e) {
+            if (e.key == roma[0]){
+                roma = roma.slice(1);
+                showThePloblem(kanji,roma)
+                if (roma ==""){
+                    nextPlobrem()
+                }
+            }
+                
+            
+        }
+
+        document.addEventListener('keypress', keypress_ivent);
